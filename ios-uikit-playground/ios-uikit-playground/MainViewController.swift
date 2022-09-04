bd621bb51aab635f270cbd867261a6d0a9adfcfe
mpilopillz <mpilo.dlamini@yahoo.com>
2022-09-04 10:20:11 +0200

    positioning the content view properly

--- ios-uikit-playground/ios-uikit-playground/MainViewController.swift
+++ ios-uikit-playground/ios-uikit-playground/MainViewController.swift
@@ -31,7 +31,9 @@ class MainViewController: UIViewController {
 class MainView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
+        self.backgroundColor = .white
         setupViews()
+        setupConstraints()
     }
     
     required init?(coder: NSCoder) {
@@ -41,6 +43,21 @@ class MainView: UIView {
     func setupViews() {
         self.addSubview(contentView)
         self.addSubview(likeButton)
+    }
+    
+    func setupConstraints() {
+        // for each view we build make sure to set this up
+        self.translatesAutoresizingMaskIntoConstraints = false
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        
+        // remeber to add isactive true else it wont show on the screen cos contraint is not active
+        contentView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 10).isActive = true
+        // assuming the negative so it does not get obstructed by the notch
+        contentView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 10).isActive = true
+        
+        contentView.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -10).isActive = true
+        contentView.bottomAnchor.constraint(equalTo: self.centerYAnchor, constant: 0).isActive = true
+        
     }
     
     // create the first view
