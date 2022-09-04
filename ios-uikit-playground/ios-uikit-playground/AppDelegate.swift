0e84684e25433bed2482829903d93c921a5e4cd0
mpilopillz <mpilo.dlamini@yahoo.com>
2022-09-04 09:57:40 +0200

    removing story boards

---
+++ ios-uikit-playground/ios-uikit-playground/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ios-uikit-playground
+//
+//  Created by Mpilo Pillz on 2022/09/04.
+//
+
+import UIKit
+import CoreData
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .systemBackground
+        window?.rootViewController = ViewController()
+        
+        return true
+    }
+
+    
+}
+
