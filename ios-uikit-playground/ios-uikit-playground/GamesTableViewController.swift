0198828cbdce4e400fc4200bbe6eaf957ada24ac
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2022-11-06 15:16:09 +0200

    adding table view and view controller reference code

---
+++ ios-uikit-playground/ios-uikit-playground/GamesTableViewController.swift
@@ -0,0 +1,64 @@
+//
+//  GamesTableViewController.swift
+//  ios-uikit-playground
+//
+//  Created by Mpilo Pillz on 2022/11/06.
+//
+
+import UIKit
+
+// Add this as rootView
+class GamesTableViewController: UIViewController {
+    
+    let games = [
+        "Pacman",
+        "Space Invaders",
+        "Space Patrol",
+    ]
+    
+    var tableView = UITableView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+}
+
+extension GamesTableViewController {
+    private func setup() {
+        setupTableView()
+    }
+    
+    private func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+}
+
+extension GamesTableViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = games[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return games.count
+    }
+}
+
+extension GamesTableViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+}
---
+++ ios-uikit-playground/ios-uikit-playground/WelcomeViewController.swift
@@ -0,0 +1,46 @@
+//
+//  WelcomeViewController.swift
+//  ios-uikit-playground
+//
+//  Created by Mpilo Pillz on 2022/11/06.
+//
+
+import UIKit
+
+
+class WelcomeViewController: UIViewController {
+    
+    let stackView = UIStackView()
+    let label = UILabel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        style()
+        layout()
+    }
+}
+
+extension WelcomeViewController {
+    func style() {
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Welcome"
+        label.font = UIFont.preferredFont(forTextStyle: .title1)
+    }
+    
+    func layout() {
+        stackView.addArrangedSubview(label)
+        
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+}
+
