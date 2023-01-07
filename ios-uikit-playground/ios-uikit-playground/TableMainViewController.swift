a1108367355db96d3f4380f09a3f7aae9d9534d2
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-01-07 15:23:10 +0200

    adding the cell

--- ios-uikit-playground/ios-uikit-playground/TableMainViewController.swift
+++ ios-uikit-playground/ios-uikit-playground/TableMainViewController.swift
@@ -20,7 +20,7 @@ class TableMainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .green
-//        tableView.dataSource = self
+        tableView.dataSource = self
         view.addSubview(tableView)
     }
     
@@ -29,16 +29,16 @@ class TableMainViewController: UIViewController {
         tableView.frame = view.bounds
     }
     
-    
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension TableMainViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Hello Pillz"
+        return cell
+    }
 }
